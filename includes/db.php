96342b1f822a6fa42f5dcf26b60c71f46709f636
Fahadul Islam <fullvollume@gmail.com>
2018-02-27 13:38:00 +0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
-$db['db_pass'] = "Mynameisfahad25%";
+$db['db_pass'] = "";
 $db['db_name'] = "cms";
 foreach($db as $key => $value){
   define(strtoupper($key), $value);
